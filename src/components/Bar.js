3b7b6e398c7eba6077b9458a1595467341307dbc
michael-fourie <17mlf1@queensu.ca>
2021-10-30 00:24:16 -0400

    font changes

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -31,17 +31,17 @@ function Bar() {
                     e.preventDefault();
                     window.location.href='https://medium.com/@cerberusdao/welcome-to-cerberusdao-the-first-decentralized-reserve-currency-that-harnesses-the-power-of-e9c2a722e7f';
                     }} 
-                    style={{ marginLeft: '3%', marginRight: '3%', color:"white", textTransform: 'none', fontSize: '12px' }} className="btn-grad">Medium</Button>
+                    style={{ marginLeft: '2%', marginRight: '3%', color:"white", textTransform: 'none', fontSize: '16px' }} className="btn-grad">Medium</Button>
                   <Button onClick = {(e) => {
                     e.preventDefault();
                     window.location.href='https://twitter.com/DaoCerberus';
                     }} 
-                     style={{ marginRight: '3%', color:"white", textTransform: 'none', fontSize: '12px' }} className="btn-grad">Twitter</Button>
+                     style={{ marginRight: '2%', color:"white", textTransform: 'none', fontSize: '16px' }} className="btn-grad">Twitter</Button>
                   <Button  onClick = {(e) => {
                     e.preventDefault();
                     window.location.href='https://t.co/EP6ouOP5Tv?amp=1';
                     }} 
-                     style={{ marginRight: '3%', color:"white", textTransform: 'none', fontSize: '12px' }} className="btn-grad">Discord</Button>
+                     style={{ marginRight: '2%', color:"white", textTransform: 'none', fontSize: '16px' }} className="btn-grad">Discord</Button>
                 </Toolbar>
             :
             <Toolbar>
