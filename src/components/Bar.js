6d85a5ebda75b5cf10bce333f7c5b0c3f52afa4e
michael-fourie <17mlf1@queensu.ca>
2021-11-02 16:04:01 -0400

    added gitbook

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -28,20 +28,25 @@ function Bar() {
                 <p className="barTitle">Cerberus DAO</p>
                 </Box> 
                   <Button onClick = {(e) => {
+                      e.preventDefault();
+                      window.location.href='https://cerberus-dao.gitbook.io/cerberusdao/';
+                      }} 
+                      style={{ marginLeft: '0.5%', marginRight: '2%', color:"#fff9a3", textTransform: 'none', fontSize: '16px' }} >GitBook</Button> 
+                  <Button onClick = {(e) => {
                     e.preventDefault();
                     window.location.href='https://medium.com/@cerberusdao/welcome-to-cerberusdao-the-first-decentralized-reserve-currency-that-harnesses-the-power-of-e9c2a722e7f';
                     }} 
-                    style={{ marginLeft: '2%', marginRight: '3%', color:"#fff9a3", textTransform: 'none', fontSize: '16px' }} >Medium</Button>
+                    style={{ marginLeft: '0.5%', color:"#fff9a3", textTransform: 'none', fontSize: '16px' }} >Medium</Button>
                   <Button onClick = {(e) => {
                     e.preventDefault();
                     window.location.href='https://twitter.com/DaoCerberus';
                     }} 
-                     style={{ marginRight: '2%', color:"#fff9a3",textTransform: 'none', fontSize: '16px' }} >Twitter</Button>
+                     style={{ marginRight: '0.5%', color:"#fff9a3",textTransform: 'none', fontSize: '16px' }} >Twitter</Button>
                   <Button  onClick = {(e) => {
                     e.preventDefault();
                     window.location.href='https://t.co/EP6ouOP5Tv?amp=1';
                     }} 
-                     style={{ marginRight: '2%', color:"#fff9a3", textTransform: 'none',fontSize: '16px' }} >Discord</Button>
+                     style={{color:"#fff9a3", textTransform: 'none',fontSize: '16px' }} >Discord</Button>
                 </Toolbar>
             :
             <Toolbar>
@@ -49,10 +54,16 @@ function Bar() {
                 <p className="barTitle">CerberusDAO</p>
                 </Box> 
                 <Button onClick = {(e) => {
+                      e.preventDefault();
+                      window.location.href='https://cerberus-dao.gitbook.io/cerberusdao/';
+                      }} 
+                      style={{ marginRight: '3%', marginLeft: '3%', color:"#fff9a3", textTransform: 'none', fontSize: '32px' }} >GitBook</Button>
+                   
+                <Button onClick = {(e) => {
                     e.preventDefault();
                     window.location.href='https://medium.com/@cerberusdao/welcome-to-cerberusdao-the-first-decentralized-reserve-currency-that-harnesses-the-power-of-e9c2a722e7f';
                     }} 
-                    style={{ marginLeft: '3%', marginRight: '3%', color:"#fff9a3",textTransform: 'none', fontSize: '32px' }} className="btn-grad">Medium</Button>
+                    style={{ marginRight: '3%', color:"#fff9a3", textTransform: 'none', fontSize: '32px' }} className="btn-grad">Medium</Button>
                   <Button onClick = {(e) => {
                     e.preventDefault();
                     window.location.href='https://twitter.com/DaoCerberus';
