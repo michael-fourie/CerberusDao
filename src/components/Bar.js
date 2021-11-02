ddd09ba5521607c66de14e1f0619ada96f0fc780
michael-fourie <17mlf1@queensu.ca>
2021-11-02 16:17:33 -0400

    mobile font fix

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -31,22 +31,22 @@ function Bar() {
                       e.preventDefault();
                       window.location.href='https://cerberus-dao.gitbook.io/cerberusdao/';
                       }} 
-                      style={{ marginLeft: '0.5%', marginRight: '2%', color:"#fff9a3", textTransform: 'none', fontSize: '16px' }} >GitBook</Button> 
+                      style={{ marginLeft: '0.5%', marginRight: '2%', color:"#fff9a3", textTransform: 'none', fontSize: '14px' }} >GitBook</Button> 
                   <Button onClick = {(e) => {
                     e.preventDefault();
                     window.location.href='https://medium.com/@cerberusdao/welcome-to-cerberusdao-the-first-decentralized-reserve-currency-that-harnesses-the-power-of-e9c2a722e7f';
                     }} 
-                    style={{ marginLeft: '0.5%', color:"#fff9a3", textTransform: 'none', fontSize: '16px' }} >Medium</Button>
+                    style={{ marginLeft: '0.5%', color:"#fff9a3", textTransform: 'none', fontSize: '14px' }} >Medium</Button>
                   <Button onClick = {(e) => {
                     e.preventDefault();
                     window.location.href='https://twitter.com/DaoCerberus';
                     }} 
-                     style={{ marginRight: '0.5%', color:"#fff9a3",textTransform: 'none', fontSize: '16px' }} >Twitter</Button>
+                     style={{ marginRight: '0.5%', color:"#fff9a3",textTransform: 'none', fontSize: '14px' }} >Twitter</Button>
                   <Button  onClick = {(e) => {
                     e.preventDefault();
                     window.location.href='https://t.co/EP6ouOP5Tv?amp=1';
                     }} 
-                     style={{color:"#fff9a3", textTransform: 'none',fontSize: '16px' }} >Discord</Button>
+                     style={{color:"#fff9a3", textTransform: 'none',fontSize: '14px' }} >Discord</Button>
                 </Toolbar>
             :
             <Toolbar>
