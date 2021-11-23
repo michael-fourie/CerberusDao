4c15d0c9c67d207d039f48434691398a4b7469b3
michael-fourie <17mlf1@queensu.ca>
2021-11-23 15:53:06 -0500

    btn

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,14 @@
 import '../style/Landing.css'
 import dog from '../media/dog.png'
 import { isMobile } from 'react-device-detect';
-
+import { Button } from '@material-ui/core';
 
 function Landing() {
+
+    const openInNewTab = (url) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+      }
    
     return(
         <div>
@@ -11,11 +16,12 @@ function Landing() {
                 <div className="vertical">
                     <h1 className="title">CerberusDAO</h1>
                     <h2 className="desc">A decentralized reserve currency powered by meme coins like SHIB, the ultra sound value of ETH, and the stable value of DAI.</h2>
+                    <Button onClick={() => openInNewTab('https://copperlaunch.com/auctions/0x053f3c35d557a76e9cb6c13a4842d9cfcddac833')} style={{margin: 'auto', color: 'white', width: '50%', fontSize: '18px', borderRadius: '32px', marginTop: '2%'}} className="auction">3DOG Auction</Button>
                 </div>
                 <div className="img">
                     <img src={dog} className="img"/>
                 </div>
-            </div>
+            </div> 
         </div>
     )
 }
